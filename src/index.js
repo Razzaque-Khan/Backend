b89c90256f3cef29892b6cb66949611f0eaac678
Abdur Razzaque <aabdurrazzaque72@gmail.com>
2025-06-29 07:57:02 +0530

    all setup configured

--- src/index.js
+++ src/index.js
@@ -1,15 +1,18 @@
 // reauire('dotenv').config({path: './env'});
-// import dotenv from 'dotenv';
 
+import dotenv from "dotenv";
 // import mongoose from "mongoose";
 // import { DB_NAME } from "./constants";
 import { app } from "./app.js";
 import connectDB from "./db/index.js";
+dotenv.config({
+  path: "./env",
+});
 
 connectDB()
   .then(() => {
     app.listen(process.env.PORT || 8000, () => {
-      console.log(`Server is runnin at port ${process.env.PORT}`);
+      console.log(`⚙️ Server is runnin at port ${process.env.PORT}`);
     });
   })
   .catch((err) => {
